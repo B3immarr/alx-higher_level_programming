9a20a3e1290a718b42b980ea47f756195ae4778f
B3immarr <brett222wambugu@gmail.com>
2023-06-26 11:13:28 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    num = 0
-    for item in range(x):
-        try:
-            print(my_list[item], end='')
-            num += 1
-        except Exception:
-            break
-    print('')
-    return num
+
+
+def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+
